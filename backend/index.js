18d12439b255343ed49d662319b3b98dd956469f
Juan Galarce <j6alarce@gmail.com>
2022-09-04 10:00:24 -0300

    Se crea la bd y se conecta a nuestro servidor, se crea las variables de entorno

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
-const express = require("express");
+import express from "express";
+import dotev from "dotenv";
+import connectDB from "./config/db.js";
 
 const app = express();
 
-console.log("desde index.js")
+dotev.config();
 
-app.listen(4000, () => {
-  console.log("Server corriendo en el port https://localhost:4000");
+connectDB();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor Corriento en el puerto https://localhost:${PORT}`);
 });
