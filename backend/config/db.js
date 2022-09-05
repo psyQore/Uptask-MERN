ff3fab9d546860f5721a069be83e79143998589c
Juan Galarce <j6alarce@gmail.com>
2022-09-05 07:45:07 -0300

    Se comienza a trabajar el routing para usuarios

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     const url = `${connection.connection.host}:${connection.connection.port}`;
     console.log(`MongoDB Contectado en: ${url}`);
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotev from "dotenv";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -8,8 +9,11 @@ dotev.config();
 
 connectDB();
 
+// Routing
+app.use("/api/users", userRoutes);
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor Corriento en el puerto https://localhost:${PORT}`);
+  console.log(`Servidor Corriento en el puerto http://localhost:${PORT}/`);
 });
