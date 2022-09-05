dbc79517c436d1163b7ff035ea2ac2088582d4cd
Juan Galarce <j6alarce@gmail.com>
2022-09-05 07:27:46 -0300

    Se crea el model de Usuario

---
+++ backend/models/User.js
@@ -0,0 +1,35 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      trim: true,
+      unique: true,
+    },
+    token: {
+      type: String,
+    },
+    confirm: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
