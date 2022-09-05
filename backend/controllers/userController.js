42784d2d45c9a814828020947c975269d8a83054
Juan Galarce <j6alarce@gmail.com>
2022-09-05 09:36:01 -0300

    Se crea la logica para evitar registros duplicados

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,15 @@
 import User from "../models/User.js";
 
 const register = async (req, res) => {
+  // Evitar registros duplicados
+  const { email } = req.body;
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    const error = new Error("Usuario ya registrado");
+    return res.status(400).json({ msg: error.message });
+  }
+
   try {
     const user = new User(req.body);
     const userStored = await user.save();
