ea5a6b65e565aa2afd90cb0342ca9e7a1fd5dd62
Juan Galarce <j6alarce@gmail.com>
2022-09-06 13:49:09 -0300

    Se crea la funcion para eliminar un proyecto por el id del creador

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -57,13 +57,34 @@ const editProject = async (req, res) => {
 
   try {
     const storedProject = await project.save();
-    res.json(storedProject)
+    res.json(storedProject);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 
-const deleteProject = async (req, res) => {};
+const deleteProject = async (req, res) => {
+  const { id } = req.params;
+
+  const project = await Project.findById(id);
+
+  if (!project) {
+    const error = new Error("No Encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (project.creator.toString() !== req.user._id.toString()) {
+    const error = new Error("Acción no Válida");
+    return res.status(401).json({ msg: error.message });
+  }
+
+  try {
+    await project.deleteOne();
+    res.json({ msg: "Proyecto Eliminado" });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const addCollaborator = async (req, res) => {};
 
