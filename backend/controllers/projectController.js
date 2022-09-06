1991a5cc7dea98fe74ac9d1797cc5cb46ef2d9eb
Juan Galarce <j6alarce@gmail.com>
2022-09-06 13:38:45 -0300

    Se crea la funcion para obtener un proyuect por el id del creador

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -2,7 +2,6 @@ import Project from "../models/Project.js";
 
 const getProjects = async (req, res) => {
   const projects = await Project.find().where("creator").equals(req.user);
-
   res.json(projects);
 };
 
@@ -18,7 +17,23 @@ const newProject = async (req, res) => {
   }
 };
 
-const getProject = async (req, res) => {};
+const getProject = async (req, res) => {
+  const { id } = req.params;
+
+  const project = await Project.findById(id);
+
+  if (!project) {
+    const error = new Error("No Encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (project.creator.toString() !== req.user._id.toString()) {
+    const error = new Error("Acción no Válida");
+    return res.status(401).json({ msg: error.message });
+  }
+
+  res.json(project);
+};
 
 const editProject = async (req, res) => {};
 
