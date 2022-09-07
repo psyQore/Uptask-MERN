a5d015e9a8c633188260785762443382d7af4a4b
Juan Galarce <j6alarce@gmail.com>
2022-09-07 11:27:02 -0300

    Se crea la funcion para obtener una tarea y al proyecto que pertenese tambien se valida

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,6 +1,46 @@
-const addTask = async (req, res) => {};
+import Project from "../models/Project.js";
+import Task from "../models/Task.js";
 
-const getTask = async (req, res) => {};
+const addTask = async (req, res) => {
+  const { project } = req.body;
+
+  const exitsProject = await Project.findById(project);
+
+  if (!exitsProject) {
+    const error = new Error("El Proyecto no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (exitsProject.creator.toString() !== req.user._id.toString()) {
+    const error = new Error("No tienes los permisos para añadir tareas");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  try {
+    const storedTask = await Task.create(req.body);
+    res.json(storedTask);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getTask = async (req, res) => {
+  const { id } = req.params;
+
+  const task = await Task.findById(id).populate("project");
+
+  if (!task) {
+    const error = new Error("Tarea no encontrada");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (task.project.creator.toString() !== req.user._id.toString()) {
+    const error = new Error("Accción no válida");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  res.json(task);
+};
 
 const updateTask = async (req, res) => {};
 
