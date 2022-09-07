a0f9f7c65f9d1c3b53dd8f6186d855ebc9a2471e
Juan Galarce <j6alarce@gmail.com>
2022-09-07 11:39:09 -0300

    Se crea la funcion para actualizar las tareas y para elimianrlas

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -42,9 +42,56 @@ const getTask = async (req, res) => {
   res.json(task);
 };
 
-const updateTask = async (req, res) => {};
+const updateTask = async (req, res) => {
+  const { id } = req.params;
 
-const deleteTask = async (req, res) => {};
+  const task = await Task.findById(id).populate("project");
+
+  if (!task) {
+    const error = new Error("Tarea no encontrada");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (task.project.creator.toString() !== req.user._id.toString()) {
+    const error = new Error("Accción no válida");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  task.name = req.body.name || task.name;
+  task.description = req.body.description || task.description;
+  task.priority = req.body.priority || task.priority;
+  task.deliveryDate = req.body.deliveryDate || task.deliveryDate;
+
+  try {
+    const storedTask = await task.save();
+    res.json(storedTask);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteTask = async (req, res) => {
+  const { id } = req.params;
+
+  const task = await Task.findById(id).populate("project");
+
+  if (!task) {
+    const error = new Error("Tarea no encontrada");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (task.project.creator.toString() !== req.user._id.toString()) {
+    const error = new Error("Accción no válida");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  try {
+    await task.deleteOne();
+    res.json({ msg: "Tarea Eliminada" });
+  } catch (error) {
+    console.log(error)
+  }
+};
 
 const changeStatus = async (req, res) => {};
 
