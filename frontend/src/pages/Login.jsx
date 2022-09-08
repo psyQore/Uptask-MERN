f42dcd00cfee350c79f863a4e1d9645763ead220
Juan Galarce <j6alarce@gmail.com>
2022-09-08 11:45:31 -0300

    Se crea el formulario de login con sus estilos

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,49 @@
-
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <h1 className=" text-sky-600 font-black text-6xl capitalize">
+        Inicia sesión y administra tus{" "}
+        <span className="text-slate-700">proyectos</span>{" "}
+      </h1>
 
-export default Login
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email de Registro"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Password de Registro"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Inicias Sesión"
+          className="bg-sky-600 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+    </>
+  );
+};
+
+export default Login;
