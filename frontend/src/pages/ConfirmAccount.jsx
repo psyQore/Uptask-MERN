44a4c766f9ebabc37412f7d1a301d8a6302b0d47
Juan Galarce <j6alarce@gmail.com>
2022-09-09 10:04:34 -0300

    Se crea la pagina de reestablete tu password y confirma tu cuenta

--- frontend/src/pages/ConfirmAccount.jsx
+++ frontend/src/pages/ConfirmAccount.jsx
@@ -1,7 +1,12 @@
 
 const ConfirmAccount = () => {
   return (
-    <div>ConfirmAccount</div>
+    <>
+      <h1 className=" text-sky-600 font-black text-6xl capitalize">
+        Confirma tu cuenta y comienza a crear tus{" "}
+        <span className="text-slate-700">proyectos</span>{" "}
+      </h1>
+    </>
   )
 }
 
--- frontend/src/pages/NewPassword.jsx
+++ frontend/src/pages/NewPassword.jsx
@@ -1,8 +1,35 @@
-
 const NewPassword = () => {
   return (
-    <div>NewPassword</div>
-  )
-}
+    <>
+      <h1 className=" text-sky-600 font-black text-6xl capitalize">
+        Reestablece tu password y no pierdas acceso a tus{" "}
+        <span className="text-slate-700">proyectos</span>{" "}
+      </h1>
 
-export default NewPassword
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="password"
+          >
+            Nuevo Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Escribe Tu Nuevo Password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Guardar Nuevo Password"
+          className="bg-sky-600 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+    </>
+  );
+};
+
+export default NewPassword;
