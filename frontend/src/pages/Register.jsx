6ac45b4094027b77b232fc55623ca1917c91c580
Juan Galarce <j6alarce@gmail.com>
2022-09-09 10:31:31 -0300

    Se validan los campos y se crea una alerta para mostrar el error

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,11 +1,27 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Alert from "../components/Alert";
 
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
+  const [alert, setAlert] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if ([name, email, password, repeatPassword].includes("")) {
+      setAlert({
+        msg: "Todos los campos son obligatorios",
+        error: true,
+      });
+      return;
+    }
+  };
+
+  const { msg } = alert;
 
   return (
     <>
@@ -13,8 +29,11 @@ const Register = () => {
         Crea tu cuenta y administra tus{" "}
         <span className="text-slate-700">proyectos</span>{" "}
       </h1>
-
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+      {msg && <Alert alert={alert} />}
+      <form
+        onSubmit={handleSubmit}
+        className="my-10 bg-white shadow rounded-lg p-10"
+      >
         <div className="my-5">
           <label
             className="uppercase text-gray-600 block text-xl font-bold"
