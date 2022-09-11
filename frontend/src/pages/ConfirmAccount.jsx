3056c2323a17dd7fcbfdee4a6979dec27b879db3
Juan Galarce <j6alarce@gmail.com>
2022-09-11 08:48:27 -0300

    Se confirma la cuenta via token

--- frontend/src/pages/ConfirmAccount.jsx
+++ frontend/src/pages/ConfirmAccount.jsx
@@ -1,13 +1,59 @@
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import Alert from "../components/Alert";
 
 const ConfirmAccount = () => {
+  const [alert, setAlert] = useState({});
+  const [accountConfirmed, setAccountConfirmed] = useState(false);
+
+  const params = useParams();
+  const { id } = params;
+
+  useEffect(() => {
+    const confirmAccount = async () => {
+      try {
+        const url = `http://localhost:4000/api/users/confirm/${id}`;
+        const { data } = await axios(url);
+
+        setAlert({
+          msg: data.msg,
+          error: false,
+        });
+
+        setAccountConfirmed(true);
+      } catch (error) {
+        setAlert({
+          msg: error.response.data.msg,
+          error: true,
+        });
+      }
+    };
+    confirmAccount();
+  }, []);
+
+  const { msg } = alert;
+
   return (
     <>
       <h1 className=" text-sky-600 font-black text-6xl capitalize">
         Confirma tu cuenta y comienza a crear tus{" "}
         <span className="text-slate-700">proyectos</span>{" "}
       </h1>
-    </>
-  )
-}
 
-export default ConfirmAccount
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+        {msg && <Alert alert={alert} />}
+        {accountConfirmed && (
+          <Link
+            className="block text-center my-5 text-slate-500 uppercase text-sm"
+            to="/"
+          >
+            Iniciar sesión
+          </Link>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ConfirmAccount;
