99fbd0de6af40d5da744d994afca290061548a19
Juan Galarce <j6alarce@gmail.com>
2022-09-12 17:52:36 -0300

    Se crea el context para la autenticacion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,22 +5,25 @@ import Register from "./pages/Register";
 import ForgetPassword from "./pages/ForgetPassword";
 import NewPassword from "./pages/NewPassword";
 import ConfirmAccount from "./pages/ConfirmAccount";
+import { AuthProvider } from "../context/AuthProvider";
 
 // console.log(import.meta.env.VITE_BACKEND_URL)
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route index element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="forget-password" element={<ForgetPassword />} />
-          <Route path="forget-password/:token" element={<NewPassword />} />
-          <Route path="confirm/:id" element={<ConfirmAccount />} />
-        </Route>
-        <Route path="/"></Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="forget-password" element={<ForgetPassword />} />
+            <Route path="forget-password/:token" element={<NewPassword />} />
+            <Route path="confirm/:id" element={<ConfirmAccount />} />
+          </Route>
+          <Route path="/"></Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
