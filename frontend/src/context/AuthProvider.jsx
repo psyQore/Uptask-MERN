da8d5f392f37f484acbf7debb75f34ff4d2d9f6a
Juan Galarce <j6alarce@gmail.com>
2022-09-13 09:07:20 -0300

    Se revisa el token en localstorage para autenticar al usuario y se guarda en el context

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,9 +1,35 @@
 import { useState, useEffect, createContext } from "react";
+import clientAxios from "../config/clinetAxios";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
+
+  useEffect(() => {
+    const authenticateUser = async () => {
+      const token = localStorage.getItem("token");
+
+      if (!token) {
+        return;
+      }
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      try {
+        const { data } = await clientAxios("/users/profile", config);
+        setAuth(data)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    authenticateUser();
+  }, []);
 
   return (
     <AuthContext.Provider
